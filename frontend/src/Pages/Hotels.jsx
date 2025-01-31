d2446870631a1ab4a87759f7fbd637138775ec3a
VocalSoda <157726925+VocalSoda@users.noreply.github.com>
2025-01-31 21:01:03 +0200

    updating urls to render ones

--- frontend/src/Pages/Hotels.jsx
+++ frontend/src/Pages/Hotels.jsx
@@ -12,7 +12,7 @@ const Hotels = () => {
     useEffect(() => {
         const fetchHotels = async () => {
             try {
-                const response = await fetch("http://localhost:3000/showlist");
+                const response = await fetch("https://travel-vite-backend.onrender.com/showlist");
                 if (!response.ok) {
                     throw new Error("Failed to fetch hotels data");
                 }
