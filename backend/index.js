9336d8f8c2d856307ad2d86d20f443f76f9362f9
VocalSoda <157726925+VocalSoda@users.noreply.github.com>
2025-01-31 19:36:05 +0200

    refactor: move sensitive data to environment variables and update database connections

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
     resave: false,
-    secret: "randomsecretkey",
+    secret: process.env.SESSION_SECRET,
     saveUninitialized: true,
     cookie: {
         maxAge: 1000 * 60 * 60,
