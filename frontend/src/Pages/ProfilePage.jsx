1a87492dde06595a3408cce23fab061f657d6cb8
VocalSoda <157726925+VocalSoda@users.noreply.github.com>
2025-03-17 21:24:57 +0200

    Remove placeholder image URL in ProfilePage component for accurate image display

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -636,7 +636,7 @@ const ProfilePage = () => {
                                     </div>
 
                                     <img
-                                        src={listingData[booking.listingID]?.Images?.[0] || "https://via.placeholder.com/150"}
+                                        src={listingData[booking.listingID]?.Images?.[0] }
                                         alt={listingData[booking.listingID]?.Listname || "Hotel"}
                                         className="rounded-lg shadow-lg max-w-full max-h-[500px] object-cover mx-auto"
                                     />
